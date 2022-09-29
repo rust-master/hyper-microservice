2f053f1d32d1b33a92c290d86a044b4a94f0dbc6
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2022-09-29 14:27:51 +0500

    microservice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::SocketAddr;
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
 fn main() {
-    println!("Hello, world!");
+    let addr: SocketAddr = ([127, 0, 0, 1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server = builder.serve(|| {
+        service_fn_ok(|_| {
+            Response::new(Body::from("Almost microservice...."))
+        })
+    });
+
+
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
 }
